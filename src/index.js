8153eba4f85729724581dc2b4f021fee19eafee9
ankitkumarsewak <ankit.kumar.sewak@gmail.com>
2024-11-19 20:29:37 +0530

    add ports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express =require("express");
 const basiRouter = require("../routers/basic");
 const app=express();
 const mongoose = require('mongoose');
+const dotenv =require("dotenv");
 
+const cor=require("cors");
+
+dotenv.config();
 app.use(express.json());
+app.use(cor());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/bus",basiRouter);
@@ -16,14 +21,14 @@ app.get("/ankit",async(req,res)=>{
       
  
 });
-
+const PORT=process.env.PORT || 5000
 mongoose.connect("mongodb+srv://Bus:1234567890@cluster0.tsacw.mongodb.net/BusAttendence?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("mongoose connect");
-    app.listen(5000,()=>{
-        console.log("server on");
+    app.listen(PORT,()=>{
+        console.log("server on :"+PORT);
     })
 }).catch((error)=>{
-    console.log("mongoose error");
+    console.log("mongoose error"+error);
 });
 
 
